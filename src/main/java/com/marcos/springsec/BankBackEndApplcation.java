8cf19ab7e2eb44575c2a83cbfd8584fcd481da50
marco <marcosxx1.ma@gmail.com>
2024-12-30 19:09:23 -0300

    first commit, start project

---
+++ src/main/java/com/marcos/springsec/BankBackEndApplcation.java
@@ -0,0 +1,14 @@
+package com.marcos.springsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBackEndApplcation {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankBackEndApplcation.class, args);
+    }
+
+}
+
